c88854a85337d4708f534131ff5902a84d62d57c
Fatima367 <princess.fatima3275@gmail.com>
2024-12-12 20:20:49 +0500

    updated

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="hidden md:mx-[32px] md:mt-0 md:visible md:block">
-      <div className="flex items-center justify-between md:space-x-auto md:-mb-12 md:mt-3 ">
+      <div className="flex items-center justify-between md:-mb-14 md:mt-2 ">
+        <div className="flex space-x-3 items-center">
         <Image src="/images/command.png" height={24} width={24} alt="command" />
-        <h2 className="font-bold text-[28px] md:mr-[550px]">
+        <h2 className="font-bold text-[24px]">
           High Fidelity Dashboard - Detail Car
         </h2>
+        </div>
         <div className="flex space-x-2">
-          <p className="text-lg font-medium text-[#666666]">Last Updated:</p>
-          <p className="text-lg font-bold -ml-12">8 Aug 2022</p>
+          <p className="text-[16px] font-medium text-[#666666]">Last Updated:</p>
+          <p className="text-[16px] font-bold -ml-10">8 Aug 2022</p>
         </div>
       </div>
     </header>
--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -5,76 +5,75 @@ export default function Navbar() {
   return (
     <nav
       className="box-border w-screen bg-white border border-[#C3D4E9] mt-0 md:mt-16
-      border-opacity-40 p-6 md:p-[30px] flex flex-col md:flex-row md:items-start 
+      border-opacity-40 p-6 md:p-5 flex flex-col md:flex-row md:items-start 
       md:justify-between items-center justify-center h-auto relative"
     >
-      {/* Logo */}
-      <div className="flex md:items-center md:space-x-0 space-x-36">
-        <Link href="/" className="h-[44px]">
-          <Image src="/images/Logo.png" width={148} height={44} alt="Logo" />
-        </Link>
-
-        <button>
-          <Image
-            src="/images/Image.png"
-            height={44}
-            width={44}
-            alt="profile"
-            className="lg:hidden block"
-          />
-        </button>
-      </div>
-
-      {/* Search bar */}
-      <div
-        className="flex items-center justify-center space-x-5
-      md:left-1/2 md:-translate-x-44 md:transform md:-mt-1 mt-4"
-      >
-        <div
-          className="flex items-center justify-between w-[280px] md:w-[492px] h-[44px]
-         lg:rounded-full rounded-md ring-1 ring-[#C3D4E9] ring-opacity-70 
-         px-4 relative"
-        >
-          <button>
-            <Image
-              src="/images/search-normal.png"
-              height={24}
-              width={24}
-              alt="search"
-            />
-          </button>
-
-          <input
-            type="text"
-            className="absolute bg-transparent md:w-[450px] h-10
-       placeholder:text-[14px] placeholder:text-[#596780] placeholder:font-medium placeholder:mx-4
-       ml-10 border-0 border-transparent focus-visible:outline-none focus-visible:ring-0"
-            placeholder="Search something here"
-          />
+      <div className="flex items-center justify-between space-x-10 -mb-5">
+        {/* Logo */}
+        <div className="flex md:items-center md:space-x-0 ml-3 mt-1">
+          <Link href="/" className="h-[44px]">
+            <Image src="/images/Logo.png" width={130} height={36} alt="Logo" />
+          </Link>
 
           <button>
             <Image
-              src="/images/filter.png"
-              height={24}
-              width={24}
-              alt="filter"
-              className="hidden md:flex"
+              src="/images/Image.png"
+              height={44}
+              width={44}
+              alt="profile"
+              className="lg:hidden block"
             />
           </button>
         </div>
-        <div
-          className=" rounded-md ring-1 ring-[#C3D4E9] ring-opacity-70 md:hidden
-      h-10 w-9 flex items-center justify-center relative"
-        >
-          <button>
-            <Image
-              src="/images/filter.png"
-              height={24}
-              width={24}
-              alt="filter"
-              className="block md:hidden"
+
+        {/* Search bar */}
+        <div className="flex items-center justify-center space-x-5 md:transform md:-mt-2 mt-4">
+          <div
+            className="flex items-center justify-between w-[280px] md:w-[420px] h-[36px]
+         lg:rounded-full rounded-md ring-1 ring-[#C3D4E9] ring-opacity-70 
+         px-4 relative"
+          >
+            <button>
+              <Image
+                src="/images/search-normal.png"
+                height={24}
+                width={24}
+                alt="search"
+              />
+            </button>
+
+            <input
+              type="text"
+              className="absolute bg-transparent md:w-[450px] h-10
+       placeholder:text-[14px] placeholder:text-[#596780] placeholder:font-medium placeholder:mx-4
+       ml-10 border-0 border-transparent focus-visible:outline-none focus-visible:ring-0"
+              placeholder="Search something here"
             />
-          </button>
+
+            <button>
+              <Image
+                src="/images/filter.png"
+                height={24}
+                width={24}
+                alt="filter"
+                className="hidden md:flex"
+              />
+            </button>
+          </div>
+          <div
+            className=" rounded-md ring-1 ring-[#C3D4E9] ring-opacity-70 md:hidden
+      h-10 w-9 flex items-center justify-center relative"
+          >
+            <button>
+              <Image
+                src="/images/filter.png"
+                height={24}
+                width={24}
+                alt="filter"
+                className="block md:hidden"
+              />
+            </button>
+          </div>
         </div>
       </div>
 
@@ -82,7 +81,7 @@ export default function Navbar() {
       <div className="md:items-center gap-5 mt-4 md:mt-0 md:ml-0 ml-5 md:mr-4 md:flex hidden">
         <Link href="/detail-car-rent">
           <div
-            className="w-11 h-11 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
+            className="w-9 h-9 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
           flex items-center justify-center"
           >
             <button>
@@ -98,8 +97,8 @@ export default function Navbar() {
 
         <Link href="/">
           <div
-            className="w-11 h-11 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
-          flex items-center justify-center"
+            className="w-9 h-9 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
+          flex items-center justify-center relative"
           >
             <button>
               <Image
@@ -109,12 +108,16 @@ export default function Navbar() {
                 alt="notification"
               />
             </button>
+            <div
+              className="rounded-full h-2 w-2 bg-red-500 z-10 absolute top-[2px]
+            right-0"
+            ></div>
           </div>
         </Link>
 
         <Link href="/">
           <div
-            className="w-11 h-11 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
+            className="w-9 h-9 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
           flex items-center justify-center"
           >
             <button>
@@ -130,7 +133,7 @@ export default function Navbar() {
 
         <Link href="/dashboard">
           <div
-            className="w-11 h-11 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
+            className="w-9 h-9 flex-none rounded-full ring-1 ring-[#C3D4E9] ring-opacity-40 
           flex items-center justify-center"
           >
             <button>
