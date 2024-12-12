ae46aae25c88681aa22cf5a575d92278e5e985c5
Fatima367 <princess.fatima3275@gmail.com>
2024-12-12 19:55:26 +0500

    updated

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="lg:bg-white bg-[#F6F7F9] w-screen bottom-0">
       <div className="lg:flex p-[80px] grid">
         <div className="space-y-[16px]">
-          <Image src="/images/Logo.png" width={148} height={44} alt="Logo" />
+          <Link href="/">
+            <Image src="/images/Logo.png" width={148} height={44} alt="Logo" />
+          </Link>
           <p className="text-[16px] font-medium text-[#131313] opacity-60">
             Our vision is to provide convenience <br /> and help increase your
             sales business.
@@ -17,18 +20,20 @@ export default function Footer() {
          space-y-8 lg:space-y-0 lg:ml-80"
         >
           <div className="flex flex-col space-y-[24px] lg:mt-0 mt-8">
-            <h2 className="text-[20px] font-semibold text-[#1A202C]">About</h2>
+            <h2 className="text-[20px] font-semibold text-[#1A202C] cursor-pointer">
+              About
+            </h2>
             <div className="space-y-[16px]">
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 How it works
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Featured
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Partnership
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Bussiness Relation
               </p>
             </div>
@@ -39,16 +44,16 @@ export default function Footer() {
               Community
             </h2>
             <div className="space-y-[16px]">
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Events
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Blog
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Podcast
               </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+              <p className="text-[16px] font-medium text-[#131313] opacity-60 cursor-pointer">
                 Invite a friend
               </p>
             </div>
@@ -58,16 +63,26 @@ export default function Footer() {
             <h2 className="text-[20px] font-semibold text-[#1A202C]">About</h2>
             <div className="space-y-[16px]">
               <p className="text-[16px] font-medium text-[#131313] opacity-60">
-                Discord
+                <Link href="https://discord.com/" target="_blank">
+                  Discord
+                </Link>
+              </p>
+
+              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+                <Link href="https://instagram.com/" target="_blank">
+                  Instagram
+                </Link>
+              </p>
+
+              <p className="text-[16px] font-medium text-[#131313] opacity-60">
+                <Link href="https://twitter.com/" target="_blank">
+                  Twitter
+                </Link>
               </p>
               <p className="text-[16px] font-medium text-[#131313] opacity-60">
-                Instagram
-              </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
-                Twitter
-              </p>
-              <p className="text-[16px] font-medium text-[#131313] opacity-60">
-                Facebook
+                <Link href="https://facebook.com/" target="_blank">
+                  Facebook
+                </Link>
               </p>
             </div>
           </div>
@@ -85,7 +100,10 @@ export default function Footer() {
         <p className="text-[16px] font-semibold text-[#1A202C]">
           ©2022 MORENT. All rights reserved
         </p>
-        <div className="space-x-[24px] flex items-center justify-center lg:ml-0 ml-10">
+        <div
+          className="space-x-[24px] flex items-center justify-center lg:ml-0 ml-10
+        hover:cursor-pointer"
+        >
           <p className="text-[16px] font-semibold text-[#1A202C]">
             Privacy Policy{" "}
           </p>
