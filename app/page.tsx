4ae61a62cdb262968a40bfe89631fba3cf43d11d
Fatima367 <princess.fatima3275@gmail.com>
2024-12-18 16:03:55 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="bg-[#F6F7F9] font-PlusJakartaSans relative">
-      <div className="flex flex-col items-center justify-center px-5 lg:px-6 mx-auto">
+      <div className="flex flex-col items-center justify-center px-5 lg:px-5 mx-auto">
         {/*Hero Section*/}
 
         <div
           className="flex lg:flex-row lg:space-x-8 items-center justify-center mt-8 flex-col lg:space-y-0 
-        space-y-6 mx-auto"
+        space-y-6 mx-auto relative"
         >
           <div
-            className="rounded-lg object-cover lg:mx-0 mx-auto
-          lg:w-[620px] lg:h-[320px] w-[310px] h-[390px] bg-blue-400 hover:scale-105 transition"
+            className="rounded-lg object-cover lg:mx-0 mx-auto relative
+          lg:w-[48%] lg:h-80 w-80 h-96 bg-blue-400 hover:scale-105 transition"
           >
             <Image
               src="/images/Ads 1.png"
@@ -24,7 +24,10 @@ export default function Home() {
               alt="Ad 1"
             />
 
-            <div className="flex flex-col space-y-4 z-10 lg:ml-6 text-white lg:-mt-80 -mt-[148px] ml-4">
+            <div
+              className="flex flex-col space-y-4 z-10 text-white absolute lg:top-5 lg:left-6
+            top-4 left-5 "
+            >
               <h2 className="text-[32px] font-semibold">
                 The Best Platform <br /> for Car Rental
               </h2>
@@ -43,19 +46,20 @@ export default function Home() {
               </Link>
             </div>
 
-            <Image
-              src="/images/image 7.png"
-              height={108}
-              width={340}
-              alt="car-image"
-              className="z-50
-              lg:ml-[150px] mb-8 lg:-mt-2 mt-8"
-            />
+            <div className="flex items-center justify-center mx-auto">
+              <Image
+                src="/images/image 7.png"
+                height={118}
+                width={340}
+                alt="car-image"
+                className="z-50 absolute lg:top-60 lg:my-auto lg:bottom-auto bottom-4"
+              />
+            </div>
           </div>
 
           <div
-            className="rounded-lg object-cover lg:w-[620px] lg:h-[320px] 
-          w-[340px] h-[400px] bg-blue-600 lg:block hidden hover:scale-105 transition"
+            className="rounded-lg lg:w-[48%] lg:h-80 w-80 h-96 bg-blue-600 lg:block hidden 
+            hover:scale-105 transition relative"
           >
             <Image
               src="/images/Ads 2.png"
@@ -64,7 +68,10 @@ export default function Home() {
               alt="Ad 2"
             />
 
-            <div className="flex flex-col space-y-4 z-10 ml-6 text-white lg:-mt-80 -mt-40">
+            <div
+              className="flex flex-col space-y-4 z-10 text-white absolute lg:top-5 lg:left-6
+            top-4 left-5"
+            >
               <h2 className="text-[32px] font-semibold">
                 The Best Platform <br /> for Car Rental
               </h2>
@@ -83,14 +90,15 @@ export default function Home() {
               </Link>
             </div>
 
-            <Image
-              src="/images/image 8.png"
-              height={108}
-              width={340}
-              alt="car-image"
-              className="z-50
-              lg:ml-[150px] mb-10 lg:-mt-4 mt-6"
-            />
+            <div className="flex items-center justify-center ">
+              <Image
+                src="/images/image 8.png"
+                height={108}
+                width={340}
+                alt="car-image"
+                className="z-50 absolute lg:top-56 lg:my-auto lg:bottom-auto bottom-4"
+              />
+            </div>
           </div>
         </div>
 
@@ -98,57 +106,56 @@ export default function Home() {
 
         {/*--------------*/}
         <div
-          className="flex flex-col lg:flex-row lg:items-center lg:justify-center gap-4 lg:mt-[68px] p-6  
-        lg:space-x-8 mb-9 items-start justify-center mx-auto mt-6"
+          className="flex flex-col lg:flex-row lg:items-center lg:justify-center gap-4 lg:mt-16  
+        lg:space-x-6 mb-9 items-center justify-center mt-6 mx-auto relative lg:max-w-[100%]"
         >
           {/* Pick-Up */}
           <div
             className="lg:flex-1 items-start justify-between bg-white shadow-md 
-          p-8 rounded-lg flex flex-col"
+          p-8 rounded-lg flex flex-col relative lg:h-40 lg:w-[36rem]"
           >
             <div className="flex-1 items-start justify-between">
-              <div className="flex space-x-3 items-center justify-start">
+              <div className="flex space-x-3 items-center justify-start lg:absolute lg:top-6 lg:left-8">
                 <div className="rounded-full h-2 w-2 bg-[#3563E9] ring-4 ring-[#3563E9] ring-opacity-30"></div>
                 <h3 className="text-lg font-semibold text-[#1A202C]">
                   Pick-Up
                 </h3>
               </div>
 
-              <div className="lg:space-x-6 lg:flex mt-4 mx-auto grid lg:space-y-0 space-y-3">
+              <div
+                className="lg:space-x-4 lg:flex mt-4 mx-auto grid lg:space-y-0 space-y-3
+              lg:absolute lg:top-14 lg:left-8"
+              >
                 <div className="space-y-2 flex flex-col">
                   <label className="text-base font-bold text-[#1A202C]">
                     Locations
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium
                    text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your city</option>
                   </select>
                 </div>
-                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40">
-                  {" "}
-                </div>
+                <div className="mx-5 w-[1px] bg-[#C3D4E9] opacity-40"> </div>
                 <div className="space-y-2 flex-col flex">
                   <label className="text-base font-bold text-[#1A202C]">
                     Date
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium
                    text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your date</option>
                   </select>
                 </div>
-                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40">
-                  {" "}
-                </div>
+                <div className="mx-5 w-[1px] bg-[#C3D4E9] opacity-40"> </div>
                 <div className="space-y-2 flex-col flex">
                   <label className="text-base font-bold text-[#1A202C]">
                     Time
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium
                    text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your time</option>
@@ -177,51 +184,50 @@ export default function Home() {
           {/* Drop-Off */}
           <div
             className="lg:flex-1 items-start justify-between bg-white shadow-md
-           p-8 rounded-lg flex flex-col"
+           p-8 rounded-lg flex flex-col relative lg:h-40 lg:w-[36rem]"
           >
             <div className="flex-1 items-start justify-between">
-              <div className="flex space-x-3 items-center justify-start">
+              <div className="flex space-x-3 items-center justify-start lg:absolute lg:top-6 lg:left-8">
                 <div className="rounded-full h-2 w-2 bg-[#3563E9] ring-4 ring-[#5CAFFC] ring-opacity-30"></div>
                 <h3 className="text-lg font-semibold text-[#1A202C]">
                   Drop-Off
                 </h3>
               </div>
 
-              <div className="lg:space-x-6 lg:flex mt-4 mx-auto grid lg:space-y-0 space-y-3">
+              <div
+                className="lg:space-x-4 lg:flex mt-4 mx-auto grid lg:space-y-0 space-y-3
+              lg:absolute lg:top-14 lg:left-8"
+              >
                 <div className="space-y-2 flex flex-col">
                   <label className="text-base font-bold text-[#1A202C]">
                     Locations
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium 
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium 
                   text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your city</option>
                   </select>
                 </div>
-                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40">
-                  {" "}
-                </div>
+                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40"> </div>
                 <div className="space-y-2 flex-col flex">
                   <label className="text-base font-bold text-[#1A202C]">
                     Date
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium 
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium 
                     text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your date</option>
                   </select>
                 </div>
-                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40">
-                  {" "}
-                </div>
+                <div className="mx-6 w-[1px] bg-[#C3D4E9] opacity-40"> </div>
                 <div className="space-y-2 flex-col flex">
                   <label className="text-base font-bold text-[#1A202C]">
                     Time
                   </label>
                   <select
-                    className="w-full lg:w-32 border-0 text-[#90A3BF] font-medium 
+                    className="w-full lg:w-[110%] border-0 text-[#90A3BF] font-medium 
                     text-xs bg-transparent lg:p-0 p-2 lg:rounded-none rounded-md"
                   >
                     <option>Select your time</option>
